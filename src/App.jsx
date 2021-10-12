45faf0eb46c57d4dcf2100e3728d4f207c79d49e
Macm91 <macm91.mc@gmail.com>
2021-10-12 16:09:23 -0400

    implemented useEffect in Exercises to set Exercises to props.exercises and allow it to map

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Register from "./Components/Register";
 import jwtDecode from 'jwt-decode';
 import Exercises from "./Components/Exercises/Exercises";
 import axios from "axios";
-// import { Redirect } from 'react-router';
-// import { Route} from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
 // import NavBar from "./Components/NavBar/NavBar";
 
 
@@ -37,6 +37,11 @@ async getExercises(){
   let response = await axios.get(`http://127.0.0.1:8000/api/wf/exercises/`)
   // .then(response => {this.setState({exercises : response.data})})
   console.log("response",response)
+
+  this.setState({
+    exercises:response.data
+  });
+  console.log ("exercise state", this.state.exercises);
 }
 
 render(){
@@ -44,10 +49,24 @@ render(){
 return(
   <div>
     {/* <NavBar className="navbar" user = {user}/>    */}
-    {/* <Exercises exercises = {this.exercises}/> */}
+    <Exercises exercises = {this.state.exercises}/>
     <Register/>
     <Login/>
     <Logout/>
+
+
+
+{/* <BrowserRouter>
+    <Switch>
+                <Route path="/register" component={Register}/>
+                <Route path="/exercises" render = {props => <Exercises {...props} exercises = {this.state.exercises}/>}/>
+                <Route path="/login" component = {Login}/>
+                <Route path="/logout" component = {Logout}/>
+                
+                
+    </Switch>
+</BrowserRouter> */}
+
   </div>
 
 );
--- src/Components/Exercises/Exercises.jsx
+++ src/Components/Exercises/Exercises.jsx
@@ -3,50 +3,39 @@ import axios from "axios";
 // import { render } from "@testing-library/react";
 
 const Exercises = (props) => {
-    const [exercises, setExercises] = useState(props.exercises);
+    console.log("exercise props", props.exercises);
+    const [exercises, setExercises] = useState([]);
+    const [ex, setEx] = useState()
     // const [searchTerm, setSearchTerm] = useState('');
+    console.log ("exercises in Exercises Comp", exercises)
    
-
-    // useEffect(() => {
-    //     getExercises();
-    // },[])
-
-    // async function getExercises(){
-    //     let response = await axios.get(`http://127.0.0.1:8000/api/wf/exercises/`)
-    //     .then(response => {setExercises(response.data)})
-    //     .then(setShowTable(true));
-    //     console.log(response.data)
-    //   }
+    useEffect(()=> {
+        setExercises(props.exercises);
+    }, [props])
+    
 
 
     
 
-    // render(){
-        
-    //     if(exercises == []){
-    //         return("Loading")
-    //     }
-        
-    //     else{
         return(
         <div className="exercise_list">
         {/* <input type="text" placeholder="search..." onChange={event => {setSearchTerm(event.target.value)}} /> */}
         <table>
             <tbody>
-                {exercises.map((val, index)=>{
-                    setExercises(val)
+                {exercises.map((val, index)=> {
                     return(
-                    <tr key={index} className="songdetails">
-                    <th> {val.name}</th>
-                    <th> {val.body_part}</th>
-                    {/* Will want a button here to add exercise to workout */}
-                    </tr>
+                        <button>
+                        <tr key={index}>
+                            <th> {val.name}</th>
+                            <th> {val.body_part}</th>
+                            <th> {val.equipment}</th>
+
+                        </tr>
+                        </button>
                     )
-                }
-                )
-                }
-                </tbody>
-                </table>
+                })}
+            </tbody>
+        </table>
         </div>
     );
 //     }
