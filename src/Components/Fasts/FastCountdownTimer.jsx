8b474441b79374a5097ae2a5c6b98772c7a9a4ad
Macm91 <macm91.mc@gmail.com>
2021-10-20 17:14:38 -0400

    implemented count to fasting countdown in fasts

--- src/Components/Fasts/FastCountdownTimer.jsx
+++ src/Components/Fasts/FastCountdownTimer.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./FastCountdownTimer.css";
-import Timer from "./Timer";
-import ControlButtons from "./ControlButtons";
+
 
 
 
@@ -9,58 +8,46 @@ import ControlButtons from "./ControlButtons";
 
     const [endTime, setEndTime] = useState("00")
     const [startTime, setStartTime] = useState(Date)
+    const [totalTime, setTotalTime] = useState()
+    const [isActive, setIsActive] = useState(false)
+    const [time, setTime] = useState(0)
+    const [count, setCount] = useState(16)
+
+
+    const increment=()=>{
+      setCount(count+1)
+    };
+
+    const decrement=()=>{
+      setCount(count-1)
+    };
+
+
+
+
     
-    const [isActive, setIsActive] = useState(false);
-    const [isPaused, setIsPaused] = useState(true);
-    const [time, setTime] = useState(0);
-
-
-
-
-    React.useEffect(() => {
-        let interval = null;
-      
-        if (isActive && isPaused === false) {
-          interval = setInterval(() => {
-            setTime((time) => time + 10);
-          }, 10);
-        } else {
-          clearInterval(interval);
-        }
-        return () => {
-          clearInterval(interval);
-        };
-      }, [isActive, isPaused]);
-      
-      const handleStart = () => {
-        setIsActive(true);
-        setIsPaused(false);
-        // put function in here to set start time 
-      };
-      
-      const handlePauseResume = () => {
-        setIsPaused(!isPaused);
-      };
-      
-      const handleReset = () => {
-        setIsActive(false);
-        setTime(0);
-      };
-
-    //   const handleSubmit = () = {
-
-    //   }
 
 return(
-    <div className="stop-watch">
-      <Timer time={time} />
-      <ControlButtons
-        active={isActive}
-        isPaused={isPaused}
-        handleStart={handleStart}
-        handlePauseResume={handlePauseResume}
-        handleReset={handleReset}
-      />
+    <div className="fastingTimer">
+      
+
+      <div className="buttons">
+        <h1>
+        <button className="increment" onClick={increment}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+          </svg>
+        </button>
+          Current Count: {count}
+        <button className="decrement" onClick={decrement}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+          </svg>
+        </button>
+        </h1>
+      </div>
+
+
     </div>
 );
 
